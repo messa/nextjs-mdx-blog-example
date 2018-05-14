ddb734b6161cc96b19079b258fb51569e4c0adda
Petr Messner <petr.messner@gmail.com>
2018-05-14 17:53:48 +0200

    prefetch top three posts

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import posts from '../posts'
 
-const PostPreview = ({ post }) => {
+const PostPreview = ({ post, prefetch }) => {
   const href = `/${post.slug}`
   return (
     <div>
-      <h2><Link href={href}><a>{post.title}</a></Link></h2>
+      <h2>
+        <Link href={href} prefetch={prefetch}><a>{post.title}</a></Link>
+      </h2>
     </div>
   )
 }
@@ -13,6 +15,8 @@ const PostPreview = ({ post }) => {
 export default () => (
   <div>
     <h1>My blog</h1>
-    {posts.map(post => <PostPreview key={post.slug} post={post} />)}
+    {posts.map((post, i) => (
+      <PostPreview key={post.slug} post={post} prefetch={i < 3} />
+    ))}
   </div>
 )
