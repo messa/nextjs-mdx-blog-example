2e5125f485169c14a3272217a416f141b3b8edcc
Petr Messner <petr.messner@gmail.com>
2018-05-20 20:17:38 +0200

    add comment

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import App, {Container} from 'next/app'
 import React from 'react'
 import PageLayout from '../components/PageLayout'
 
+// Override the App class to put layout component around the page contents
+// https://github.com/zeit/next.js#custom-app
+
 export default class MyApp extends App {
 
   render () {
