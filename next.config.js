efe256b7a1db80f8b4f61d275e8f16b0b6f45277
Petr Messner <petr.messner@gmail.com>
2018-05-20 18:58:09 +0200

    add exportPathMap: do not generate out/index/index.html

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withMDX = require('@zeit/next-mdx')()
 module.exports = withMDX({
   pageExtensions: ['js', 'mdx'],
+  exportPathMap: function(defaultPathMap) {
+    const pathMap = Object.assign({}, defaultPathMap)
+    delete pathMap['/index']
+    return pathMap
+  }
 })
